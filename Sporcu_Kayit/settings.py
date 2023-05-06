ea7ddbcc474429bbd6bf79abaa36b10e27b11685
ugurokan <132758270+ugurokan@users.noreply.github.com>
2023-05-06 16:46:30 +0300

    asd

--- Sporcu_Kayit/settings.py
+++ Sporcu_Kayit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-=c5hsk-sf&@phot9#9*vm28!j8f8m4p1_v*!gfb$r)8^u6f3rd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['andiugur.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
@@ -116,8 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'public/static/')
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
